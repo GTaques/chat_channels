ca77b9d799c30106d889934f7122fc5d23f16b06
Gabriel Henrique Taques <gabrielpctaques@gmail.com>
2018-08-17 14:35:39 -0300

    Persist try

--- chat/views.py
+++ chat/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.utils.safestring import mark_safe
+from .models import Message
 import json
 
 def index(request):
@@ -10,3 +11,11 @@ def room(request, room_name):
     return render(request, 'chat/room.html', {
         'room_name_json': mark_safe(json.dumps(room_name))
     })
+
+def log_create(request):
+    if request.method == 'POST':
+        message = Message()
+        message.text = request.POST['message_text']
+        message.save()
+    else:
+        return render(request, 'chat/log.html', {})
